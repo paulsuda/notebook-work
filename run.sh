684af43df433b2836cab4b04bc091d2e91ccc42b
Paul Suda <paulsuda@gmail.com>
2016-08-07 17:52:01 -0500

    Using npm for some things.

--- run.sh
+++ run.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 #
 
+eval $(docker-machine env default)
+
+notebook_url="http://${DOCKER_HOST:7}/"
+
+echo '*****'
+echo '*****'
+echo "***** Visit $notebook_url"
+echo '*****'
+echo '*****'
+
+open $notebook_url
+
 docker run \
   --publish=8888:8888 \
   --rm --tty --interactive \
   --volume=`pwd`/notebooks:/notebooks \
   gcr.io/tensorflow/tensorflow
+
+
