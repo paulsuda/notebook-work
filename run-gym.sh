565ce06198867d81694f7f2609b70164609f9ca2
Paul Suda <paulsuda@gmail.com>
2017-06-28 23:19:06 -0500

    Old scripts gone

--- run-gym.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-
-docker run \
-  --rm -ti \
-  -p 18888:8888 -p 15900:5900 \
-  --volume=`pwd`/notebooks:/mnt/notebooks \
-  psuda/jupyter-gym-vnc
--- run-tf.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-
-docker run \
-  --rm -ti \
-  -p 18888:8888 \
-  --volume=`pwd`/notebooks:/notebooks \
-  tensorflow/tensorflow $*
